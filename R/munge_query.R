d11c497b8386c8d334efe768ad16b2cdf1c1f55b
Taavi Päll <tapa741@gmail.com>
2018-10-02 10:38:22 +0300

    prj query

--- R/munge_query.R
+++ R/munge_query.R
@@ -3,13 +3,54 @@ library(tidyverse)
 library(skimr)
 result <- read_csv("output/metagenome_query_2018-09-30.csv")
 skim(result)
+
 result <- mutate(result, Experiment_name = str_replace_all(Experiment_name, "\"", ""))
 
-result_selected <- select(result, which(!near(summarise_all(result, n_distinct), 1)))
+result_selected <- select(result, which(!summarise_all(result, n_distinct) <= 1))
 results_wgs <- filter(result_selected, library_strategy == "WGS")
 results_wgs_paired <- filter(results_wgs, library_layout == "PAIRED")
 
+
+#' Number of submitted samples
 ggplot(data = result_selected) +
   geom_histogram(mapping = aes(x = CreateDate), bins = 30) +
   facet_wrap(~library_layout)
+
+# Whoaa.., one project contributes mos of the samples
+results_wgs_paired %>% 
+  group_by(Bioproject) %>% 
+  summarise(N = n()) %>% 
+  arrange(desc(N))
+
+#' Number of bioprojects
+result_selected %>% 
+  group_by(Bioproject, library_layout) %>% 
+  summarise(CreateDate = min(CreateDate)) %>% 
+  ggplot() +
+  geom_histogram(mapping = aes(x = CreateDate), bins = 10) +
+  facet_wrap(~library_layout)
+
+#' Number of spots per run
+results_wgs_paired %>% 
+  ggplot() +
+  geom_histogram(aes(Run_total_spots), bins = 30) +
+  scale_x_log10()
+
+#' Filter by spots, remove samples with less than 1e5 spots
+results_wgs_paired_filtered <- results_wgs_paired %>% 
+  filter(Run_total_spots > 1e5)
+
+skim(results_wgs_paired_filtered)
+
+#' 
+results_wgs_paired_filtered %>% 
+  group_by(Bioproject) %>% 
+  summarise(N = n()) %>% 
+  arrange(desc(N))
+
+filter(results_wgs_paired_filtered, Bioproject == "PRJNA290729") %>% 
+  pull(Run_acc) %>% 
+  unique()
+
+
 
---
+++ R/prjna361402_query.R
@@ -0,0 +1,34 @@
+
+library(tidyverse)
+library(entrezquery)
+
+#' Check if we already have query data available.
+
+if (file.exists("output/PRJNA361402_query_2018-10-02.csv")) {
+  prj_wgs <- read_csv("output/PRJNA361402_query_2018-10-02.csv")
+} else {
+  library(xml2)
+  library(XML)
+  source("R/sra_xml_parsers.R")
+  
+  q <- "PRJNA361402"
+  prj <- entrezquery::entrez_docsums(query = q, db = "sra", retmax = 1000)
+  prj_fixed  <- mutate(prj, ExpXml = map(ExpXml, fix_xml),
+                       Runs = map(Runs, fix_xml))
+  
+  parse_expxml_safely <- safely(parse_expxml, quiet = FALSE)
+  parse_runs_xml_safely <- safely(parse_runs_xml, quiet = FALSE)
+  
+  prj_parsed  <- prj_fixed %>% 
+    mutate(ExpXml = map2(ExpXml, Id, parse_expxml_safely),
+           Runs = map(Runs, parse_runs_xml_safely))
+  prj_parsed <- prj_parsed %>% mutate_at(vars(ExpXml, Runs), map, "result")
+  sets_with_probs <- filter(prj_parsed, map_lgl(ExpXml, is.null) | map_lgl(Runs, is.null))
+  sets_ok <- filter(prj_parsed, !map_lgl(ExpXml, is.null), !map_lgl(Runs, is.null))
+  sets_unnested <- unnest(sets_ok)
+  results_wgs <- filter(sets_unnested, library_strategy == "WGS")
+  write_csv(results_wgs, glue::glue("output/PRJNA361402_query_{Sys.Date()}.csv"))
+}
+
+DT::datatable(prj_wgs)
+
