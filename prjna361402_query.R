4211ec09b0e7f0748b1028ec26045953c63d2c54
Taavi Päll <tapa741@gmail.com>
2018-10-03 07:59:52 +0300

    eutils

--- prjna361402_query.R
+++ prjna361402_query.R
@@ -1,43 +1,19 @@
-#'---
-#' author: "Taavi Päll"
-#' date: "2018-10-01"
-#' output: github_document
-#' always_allow_html: yes
-#'---
 
-library(tidyverse)
+library(httr)
+library(rvest)
+search <- GET("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=PRJNA361402&retmax=1000&usehistory=y")
 
-#' Check if we already have query data available.
+conts <- content(search)
+web <- xml_find_first(conts, "WebEnv") %>% xml_text()
+key <- xml_find_first(conts, "QueryKey") %>% xml_text()
 
-if (file.exists("output/PRJNA361402_query_2018-10-02.csv")) {
-  prj_wgs <- read_csv("output/PRJNA361402_query_2018-10-02.csv")
-} else {
-  library(xml2)
-  library(XML)
-  source("R/sra_xml_parsers.R")
-  
-  q <- "PRJNA361402"
-  prj <- entrezquery::entrez_docsums(query = q, db = "sra", retmax = 1000)
-  prj_fixed  <- mutate(prj, ExpXml = map(ExpXml, fix_xml),
-                       Runs = map(Runs, fix_xml))
-  
-  parse_expxml_safely <- safely(parse_expxml, quiet = FALSE)
-  parse_runs_xml_safely <- safely(parse_runs_xml, quiet = FALSE)
-  
-  prj_parsed  <- prj_fixed %>% 
-    mutate(ExpXml = map2(ExpXml, Id, parse_expxml_safely),
-           Runs = map(Runs, parse_runs_xml_safely))
-  prj_parsed <- prj_parsed %>% mutate_at(vars(ExpXml, Runs), map, "result")
-  sets_with_probs <- filter(prj_parsed, map_lgl(ExpXml, is.null) | map_lgl(Runs, is.null))
-  sets_ok <- filter(prj_parsed, !map_lgl(ExpXml, is.null), !map_lgl(Runs, is.null))
-  sets_unnested <- unnest(sets_ok)
-  results_wgs <- filter(sets_unnested, library_strategy == "WGS")
-  write_csv(results_wgs, glue::glue("output/PRJNA361402_query_{Sys.Date()}.csv"))
-}
+fetch <- GET(glue::glue("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&query_key={key}&WebEnv={web}"))
+fetched_contents <- content(fetch)
+fechild <- xml_children(fetched_contents)
+fechild[[1]] %>% xml_find_first("SAMPLE/SAMPLE_ATTRIBUTES") %>% xml_text()
 
-knitr::kable(head(prj_wgs)) %>% 
-  kableExtra::kable_styling()
 
-prj_wgs
-length(unique(prj_wgs$Experiment_name))
-length(unique(prj_wgs$Biosample))
+res <- GET("http://trace.ncbi.nlm.nih.gov/Traces/sra/sra.cgi?save=efetch&db=sra&rettype=runinfo&term=PRJNA361402")
+runinfo <- content(res, as = "text", encoding = "UTF8")
+read_csv(runinfo) %>% 
+  DT::datatable()
