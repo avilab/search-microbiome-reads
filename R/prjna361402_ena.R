147853863469c2fc03f2e0f1462efb784c6bcf55
Taavi Päll <tapa741@gmail.com>
2018-11-19 13:08:15 +0200

    remote ftp download

--- R/prjna361402_ena.R
+++ R/prjna361402_ena.R
@@ -7,15 +7,25 @@ library(readxl)
 project <- "PRJNA361402"
 study <- "SRP100518"
 
-#' retrieve fastq file info and urls
+#' Retrieve fastq file info and urls
 fq <- GET(glue("http://www.ebi.ac.uk/ena/data/warehouse/filereport?accession={study}&result=read_run"))
-fq_ftp <- content(fq, encoding = "UTF8") %>% 
-  read_delim(delim = "\t")
+fq_ftp <- content(fq, encoding = "UTF-8") %>% read_delim(delim = "\t")
+
+samples <- select(fq_ftp, experiment_title, run_accession, library_name, read_count, base_count, fastq_bytes, fastq_md5, fastq_ftp)
+
+samples %>% 
+  separate(fastq_ftp, into = c("fq1", "fq2"), sep = ";") %>% 
+  separate(fastq_bytes, into = c("fq1_bytes", "fq2_bytes"), sep = ";") %>%
+  separate(fastq_md5, into = c("fq1_md5", "fq2_md5"), sep = ";") %>% 
+  rename(sample = run_accession) %>% 
+  write_delim("samples_remote.tsv", delim = "\t")
+
+
 
 #' Runs and samples
 url <- glue("https://www.ebi.ac.uk/ena/data/view/{project}&portal=read_run&display=xml")
 resp <- GET(url)
-read_run <- content(resp, encoding = "UTF8")
+read_run <- content(resp, encoding = "UTF-8")
 contents <- read_run %>% 
   read_xml() %>% 
   xml_contents()
